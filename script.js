1add484e9e69c711954b32b724dd062011f67ec3
Salomao-Faleta <faletas41@gmail.com>
2024-01-31 03:35:21 -0300

    Tamanho de caixa
    Alterei o titulo e tamanho da imagem

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const imagemVisualizacao = document.querySelector('#imagem-visualizacao')
+const imagemVisualizacao = document.querySelector('#imagem-visualizacao');
+const tituloProduto = document.querySelector('#titulo-produto');
 
 const verdeCipreste = {
     nome: 'Verde-cipreste',
@@ -26,13 +27,42 @@ const rosaClaro = {
 }
 
 const minhaLista = [verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro];
+const opcoesTamanho = ['41 mm', '45 mm'];
 
 let imagemSelecionada = 1;
+let tamanhoSelecionado = 1;
 
-function trocarImagem(){
+
+function trocarImagem() {
+
     //trocando imagem atraves do id do elemento
     const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id;
     imagemSelecionada = idOpcaoSelecionada.charAt(0);
 
     imagemVisualizacao.src = `./imagens/opcoes-cores/imagens-azul-inverno/imagem-${imagemSelecionada}.jpeg`
 }
+
+function trocarTamnho() {
+    // atualizar variavel de controle do tamnho da caixa
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-tamanho"]:checked').id;
+    tamanhoSelecionado = idOpcaoSelecionada.charAt(0)
+
+    // mudar o titulo do produto
+    tituloProduto.innerHTML = `Pulseira loop esportiva azul-inverno para caixa de ${opcoesTamanho[tamanhoSelecionado]}`
+
+
+    //mudar o tamnho de acordo com a opção
+    // Com operador ternário
+    tamanhoSelecionado == 0 ? imagemVisualizacao.classList.add('caixa-pequena') : imagemVisualizacao.classList.remove('caixa-pequena');
+
+    //Modo padão - (mais "complexo")
+    // if(opcoesTamanho[tamanhoSelecionado] === '41 mm'){
+    //     imagemVisualizacao.classList.add('caixa-pequena');
+    // }else{
+    //     imagemVisualizacao.classList.remove('caixa-pequena');
+    // }
+
+    
+}
+
+
